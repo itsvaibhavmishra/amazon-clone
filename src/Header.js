f815095a8c9a16c0dc3e4f0d858f17e3ab13fdad
Vaibhaw <vaibhavmishra658@gmail.com>
2023-01-30 11:43:57 +0530

    Header Logo, Search and Items

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,33 @@ import React from "react";
 
 function Header() {
     return(
-        <div>
-            This is header
+        <div className="header">
+            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" className="header__logo" alt="amazon logo"/>
+        
+        <div className="header__search">
+            <input type="text" className="header__searchIn" placeholder="Search"/>
+            {/* Logo */}
+        </div>
+
+        <div className="header__items">
+           
+            <div className="item__option">
+                <span className="first__line">Hello,</span>
+                <span className="second__line">Sign In</span>
+            </div>
+           
+            <div className="item__option">
+                <span className="first__line">Returns</span>
+                <span className="second__line">& Orders In</span>
+            </div>
+            
+            <div className="item__option">
+                <span className="first__line">Your</span>
+                <span className="second__line">Prime In</span>
+            </div>
+
+        </div>
+
         </div>
     )
 }
