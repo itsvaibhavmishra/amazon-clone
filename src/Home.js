71c243f4adc237eb53b93ae588f63af98e274c7e
Vaibhaw <vaibhavmishra658@gmail.com>
2023-02-02 13:18:36 +0530

    Added test product and css

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./Home.css";
+import "./assists/Product";
+import Product from "./assists/Product";
 
 function Home() {
 
     return(
-        <div className="container">
-            <img src="https://m.media-amazon.com/images/I/710i3fow2GL._SX3000_.jpg" alt="homeLogo" className="home__image" />
+        <div className="home">
+            <div className="container">
+                <img src="https://m.media-amazon.com/images/I/710i3fow2GL._SX3000_.jpg" alt="homeLogo" className="home__image" />
+            
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+                
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </div>
+    
         </div>
     )
 }
