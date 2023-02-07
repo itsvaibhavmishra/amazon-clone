bc9a4500f65dc8561907d0d8653268ef372d6f61
Vaibhaw <vaibhavmishra658@gmail.com>
2023-02-07 12:16:31 +0530

    Added props for including individual products

--- src/Home.js
+++ src/Home.js
@@ -11,24 +11,63 @@ function Home() {
                 <img src="https://m.media-amazon.com/images/I/710i3fow2GL._SX3000_.jpg" alt="homeLogo" className="home__image" />
             
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                    info= "iQOO Z6 Lite 5G by vivo (Stellar Green, 4GB RAM, 64GB Storage) | World's First Snapdragon 4 Gen 1 | 120Hz Refresh Rate | 5000mAh Battery"
+                    price="13,499"
+                    rating={4}
+                    image= "https://m.media-amazon.com/images/I/61VbKHdE0rL._SX569_.jpg" />
+                    
+                    <Product 
+                    info= "boAt Rockerz 550 Bluetooth Wireless Over Ear Headphones with Upto 20 Hours Playback, 50MM Drivers, Soft Padded Ear Cushions and Physical Noise Isolation with Mic (Maroon Maverick)"
+                    price="1,999"
+                    rating={4}
+                    image= "https://m.media-amazon.com/images/I/41X8VCZXYuL._SX300_SY300_QL70_FMwebp_.jpg" />
+
+                    <Product 
+                    info= "Logitech MK470 Slim Wireless Keyboard and Mouse Combo - Modern Compact Layout, Ultra Quiet, 2.4 GHz USB Receiver, Plug n' Play Connectivity, Compatible with Windows - White"
+                    price="4,432"
+                    rating={5}
+                    image= "https://m.media-amazon.com/images/I/41sp7pnzHUL._SX300_SY300_QL70_FMwebp_.jpg" />
+                    
                 </div>
                 
                 <div className="home__row">
-                    <Product />
-                    <Product />
+                    <Product 
+                    info="Dell New Alienware x15 R2 Gaming Laptop, Intel i9-12900H, Win11+MSO'21 , 32GB LP DDR5, 1TB SSD, NVIDIA RTX 3080 Ti (16GB GDDR6)"
+                    price="3,56,600"
+                    rating={4}
+                    image="https://m.media-amazon.com/images/I/61Zwojyjm5L._SX679_.jpg" />
+                    <Product 
+                    info=""
+                    price=""
+                    rating={0}
+                    image="" />
                 </div>
 
                 <div className="home__row">
-                    <Product />
+                    <Product 
+                    info=""
+                    price=""
+                    rating={0}
+                    image="" />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                    info=""
+                    price=""
+                    rating={0}
+                    image="" />
+                    <Product 
+                    info=""
+                    price=""
+                    rating={0}
+                    image="" />
+                    <Product 
+                    info=""
+                    price=""
+                    rating={0}
+                    image="" />
                 </div>
             </div>
     
--- src/assists/Product.js
+++ src/assists/Product.js
@@ -3,26 +3,25 @@ import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 
-function Product() {
+function Product({info, price, rating, image}) {
 
     return(
         <div className="product">
             <div className="product__info">
-                <p>iQOO Z6 Lite 5G by vivo (Stellar Green, 4GB RAM, 64GB Storage) | World's First Snapdragon 4 Gen 1 | 120Hz Refresh Rate | 5000mAh Battery</p>
+                <p>{info}</p>
                 <p className="product__price">
                     <small>₹</small>
-                    <strong>13,499</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="ratings">
-                    <StarIcon className="star__icon"/>
-                    <StarIcon className="star__icon"/>
-                    <StarIcon className="star__icon"/>
-                    <StarIcon className="star__icon"/>
-                    <StarBorderIcon className="star__icon"/>
+                {Array(rating)
+                .fill()
+                .map((_, i) => (<StarIcon className="star__icon"/> ))}
+                    {/* <StarBorderIcon className="star__icon"/> */}
                 </div>
             </div>
 
-            <img src="https://m.media-amazon.com/images/I/61VbKHdE0rL._SX569_.jpg" alt="" />
+            <img src={image} alt="" />
 
             <button>Add to Cart</button>
         </div>
