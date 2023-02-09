bbeaa72a2ec800143b9cc449a6e879a0a2102123
Vaibhaw <vaibhavmishra658@gmail.com>
2023-02-09 19:20:56 +0530

    Added routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+      <Routes>
+        {/* when inside of '/' show header and home */}
+        <Route path='/' element={
+          <> <Header/> <Home/></>
+        }/>
+
+        <Route path='/checkout' element={
+          <> <Header/> Hello This is Checkout Page <Home/></>
+        }/>
+
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
