349590f515eb2ba74eea2a39bb7cd79fcbadfab6
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-02-18 13:47:15 +0530

    Firebase SignIn/Up System

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEeE7eKH6vYwkBkgi4MoqBqw63IcVkXnY",
+    authDomain: "clone-7ad5f.firebaseapp.com",
+    projectId: "clone-7ad5f",
+    storageBucket: "clone-7ad5f.appspot.com",
+    messagingSenderId: "719252570345",
+    appId: "1:719252570345:web:ca70dacb8bacb1dda73078",
+    measurementId: "G-98FMEWBEQ5"
+};
+
