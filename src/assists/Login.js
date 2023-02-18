5ccf87be511257200f1be33ea7bf1fcad175b405
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-02-18 13:32:44 +0530

    Initializing Firebase

--- src/assists/Login.js
+++ src/assists/Login.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // firebase login system
+    }
+
+    const signUp = e => {
+        e.preventDefault();
+
+        // firebase register system
+    }
 
     return(
         <div className="login">
@@ -16,12 +31,13 @@ function Login() {
 
                 <form action="">
                     <h5>Email</h5>
-                    <input type="email"  />
+                    {/* e is the event that sends the value being typed by user */}
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className="signin__button">Sign in</button>
+                    <button className="signin__button" onClick={signIn} type="submit">Sign in</button>
                 </form>
 
                 <p className="abtncs">By continuing, you agree to Amazon's <a className="tncs" href="https://www.amazon.in/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=200545940">Condition of Use </a>
@@ -32,7 +48,7 @@ function Login() {
                 <h5 className="hrL">
                     <span>New to Amazon?</span>
                 </h5>
-                <button className="signup__button">Create your Amazon account</button>
+                <button className="signup__button" onClick={signUp}>Create your Amazon account</button>
         </div>
     )
 }
